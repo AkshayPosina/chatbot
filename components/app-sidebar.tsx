9f3b40df639bf1294530f6a5b4fc55ff89076b7c
AkshayPosina <akshaypruthi.posina@gmail.com>
2025-08-30 11:25:04 +0530

    fix: disable assistant-ui link in sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -19,16 +19,15 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
-                <Link href="https://assistant-ui.com" target="_blank">
-                  <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                    <MessagesSquare className="size-4" />
-                  </div>
-                  <div className="flex flex-col gap-0.5 leading-none">
-                    <span className="font-semibold">assistant-ui</span>
-                  </div>
-                </Link>
-              </SidebarMenuButton>
+            {/* CORRECTED THIS BUTTON - REMOVED LINK */}
+            <SidebarMenuButton size="lg">
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                <MessagesSquare className="size-4" />
+              </div>
+              <div className="flex flex-col gap-0.5 leading-none">
+                <span className="font-semibold">assistant-ui</span>
+              </div>
+            </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
